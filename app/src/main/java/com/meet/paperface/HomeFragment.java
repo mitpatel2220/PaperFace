960be536a89363ef0efc8564b74e8641374cb9c1
meetpatel2220 <mit202220@gmail.com>
2020-01-09 22:37:22 +0530

    Payment Activity & Home Fragment done

--- app/src/main/java/com/meet/paperface/HomeFragment.java
+++ app/src/main/java/com/meet/paperface/HomeFragment.java
@@ -1,20 +1,29 @@
 package com.meet.paperface;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    Button edit, ok, done;
+
+    EditText edit_how, edit_extra;
+
+    TextView edit_page, edit_rs;
 
 
     public HomeFragment() {
@@ -25,11 +34,128 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_home, container, false );
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated( view, savedInstanceState );
+        super.onViewCreated(view, savedInstanceState);
+
+        edit = view.findViewById(R.id.edit);
+        ok = view.findViewById(R.id.ok);
+        done = view.findViewById(R.id.done);
+
+        edit_how = view.findViewById(R.id.edit_How);
+        edit_extra = view.findViewById(R.id.edit_Extra);
+
+        edit_page = view.findViewById(R.id.edit_Pages);
+        edit_rs = view.findViewById(R.id.edit_Rs);
+
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                float total_rs;
+                String pages = edit_how.getText().toString();
+                String extrapages = edit_extra.getText().toString();
+
+                if (extrapages.isEmpty()) {
+
+
+                    extrapages = "0";
+
+                }
+
+
+                String pages1 = edit_how.getText().toString();
+                int total_pages;
+                if (pages1.isEmpty()) {
+
+                    Toast.makeText(getActivity(), "Please enter Bunch of pages", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    int pages_int = Integer.parseInt(pages);
+                    int extra_int = Integer.parseInt(extrapages);
+
+
+                    total_pages = (pages_int * 100) + extra_int;
+
+                    edit_page.setText(total_pages + "");
+
+                    total_rs = (float) ((total_pages * 55) / 100.0);
+
+                    String x = total_rs + "0";
+                    edit_rs.setText(x);
+
+
+                    Intent intent = new Intent(getContext(), PaymentActivity.class);
+                    intent.putExtra("total_page", total_pages + "");
+                    intent.putExtra("total_rs", x);
+
+
+                }
+
+
+            }
+        });
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                float total_rs;
+                String pages = edit_how.getText().toString();
+                String extrapages = edit_extra.getText().toString();
+
+                if (extrapages.isEmpty()) {
+
+
+                    extrapages = "0";
+
+                }
+
+
+                String pages1 = edit_how.getText().toString();
+                int total_pages;
+                if (pages1.isEmpty()) {
+
+                    Toast.makeText(getActivity(), "Please enter Bunch of pages", Toast.LENGTH_SHORT).show();
+
+                }
+                else if(edit_page.getText().toString().isEmpty()) {
+                    Toast.makeText(getActivity(), "Please Touch on ok", Toast.LENGTH_SHORT).show();
+
+
+                }
+                    else
+                 {
+                    int pages_int = Integer.parseInt(pages);
+                    int extra_int = Integer.parseInt(extrapages);
+
+
+                    total_pages = (pages_int * 100) + extra_int;
+
+
+                    total_rs = (float) ((total_pages * 55) / 100.0);
+
+                    String x = total_rs + "0";
+
+
+                    Intent intent = new Intent(getContext(), PaymentActivity.class);
+                    intent.putExtra("total_page", total_pages + "");
+                    intent.putExtra("total_rs", x);
+                      startActivity(intent);
+
+
+                }
+
+
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/meet/paperface/PaymentActivity.java
+++ app/src/main/java/com/meet/paperface/PaymentActivity.java
@@ -1,14 +1,162 @@
 package com.meet.paperface;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class PaymentActivity extends AppCompatActivity {
+
+    EditText name_Payment, Room_number_Payment, hostel_name_Payment, others_Payment, mobileNo_payment;
+
+    RadioButton online_Payment_Radio, cod_Radio, clicked_radio;
+
+    Button Place_order;
+
+    RadioGroup radioGroup;
+
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+
+
+        Intent intent = getIntent();
+        final String page = intent.getStringExtra("total_page");
+        final String rs = intent.getStringExtra("total_rs");
+
+
+        name_Payment = findViewById(R.id.name_payment);
+        mobileNo_payment = findViewById(R.id.mobile_no_payment);
+        Room_number_Payment = findViewById(R.id.room_number_Payment);
+        hostel_name_Payment = findViewById(R.id.hostel_name_payment);
+        others_Payment = findViewById(R.id.others_payment);
+
+        online_Payment_Radio = findViewById(R.id.payOnline_Payment_Radio);
+        cod_Radio = findViewById(R.id.cod_Payment_Radio);
+
+        radioGroup = findViewById(R.id.radio_grp);
+
+        Place_order = findViewById(R.id.place_Order_Payment);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Orders");
+        final String myuid = firebaseUser.getUid().toString();
+
+
+        Place_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String name = name_Payment.getText().toString();
+                String mobilenumber = mobileNo_payment.getText().toString();
+                String Room_no = Room_number_Payment.getText().toString();
+                String hostelName = hostel_name_Payment.getText().toString();
+                String otherPayment = others_Payment.getText().toString();
+
+                int id = radioGroup.getCheckedRadioButtonId();
+                clicked_radio = findViewById(id);
+
+                String click = clicked_radio.getText().toString();
+
+                if(otherPayment.isEmpty()){
+
+                    otherPayment="--";
+                }
+
+                if (name.isEmpty()) {
+
+                    Toast.makeText(PaymentActivity.this, "Please enter Your Name", Toast.LENGTH_SHORT).show();
+                } else if (mobilenumber.isEmpty()) {
+                    Toast.makeText(PaymentActivity.this, "Please enter Your Mobile No.", Toast.LENGTH_SHORT).show();
+
+
+                } else if (Room_no.isEmpty()) {
+                    Toast.makeText(PaymentActivity.this, "Please enter Your Room No.", Toast.LENGTH_SHORT).show();
+
+
+                } else if (hostelName.isEmpty()) {
+
+                    Toast.makeText(PaymentActivity.this, "Please enter Your Hostel Name", Toast.LENGTH_SHORT).show();
+
+                }  else {
+
+                    if(click.equals("Pay Online")){
+
+
+
+
+
+                    }else {
+
+                        HashMap<String,String> map=new HashMap<>();
+                        map.put("hostelname",hostelName);
+                        map.put("mobileno",mobilenumber);
+                        map.put("name",name);
+                        map.put("other",otherPayment);
+                        map.put("payment","Payment Left");
+                        map.put("roomno",Room_no);
+                        map.put("totalpage",page);
+                        map.put("totalrs",rs);
+
+
+
+                        databaseReference.child(myuid).setValue(map).addOnCompleteListener(PaymentActivity.this, new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if(task.isSuccessful()){
+
+                                    Toast.makeText(PaymentActivity.this, "Your Order is successfully Placed", Toast.LENGTH_SHORT).show();
+
+
+                                }else {
+
+
+                                }
+
+
+
+
+                            }
+                        });
+
+
+
+
+                    }
+
+
+
+                }
+
+
+            }
+        });
+
+
     }
+
+
 }
