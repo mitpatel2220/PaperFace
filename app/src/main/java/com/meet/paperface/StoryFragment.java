cfc08d4d6a2e8034b9bdd7be35708559179ce9fc
Deepak681 <47095705+Deepak681@users.noreply.github.com>
2020-01-10 10:26:31 +0530

    Login, Registration, ShopKeeper, Payment and Home Fragment activities have done.

--- app/src/main/java/com/meet/paperface/StoryFragment.java
+++ app/src/main/java/com/meet/paperface/StoryFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,15 +15,27 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StoryFragment extends Fragment {
+    RecyclerView recyclerView;
+    DatabaseReference databaseReference;
+    Story_Adapter story_adapter;
+    List<Story_Model> models;
 
-Button b1;
     public StoryFragment() {
         // Required empty public constructor
+        
     }
 
 
@@ -35,18 +49,30 @@ Button b1;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        b1=view.findViewById(R.id.b1);
-
-        b1.setOnClickListener(new View.OnClickListener() {
+        recyclerView = view.findViewById( R.id.recycler_frag );
+        recyclerView.setHasFixedSize( true );
+        recyclerView.setLayoutManager( new LinearLayoutManager( getContext() ) );
+        models = new ArrayList<>(  );
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        
+        databaseReference.addValueEventListener( new ValueEventListener() {
             @Override
-            public void onClick(View view) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+                    Story_Model story_model = dataSnapshot1.getValue(Story_Model.class);
+                    models.add( story_model );
+                }
+                story_adapter = new Story_Adapter( getContext(), models );
+                recyclerView.setAdapter( story_adapter );
+            }
 
-                Toast.makeText(getActivity(), "story", Toast.LENGTH_SHORT).show();
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText( getContext(), databaseError.getMessage(), Toast.LENGTH_SHORT ).show();
 
             }
-        });
-
+        } );
+        
 
     }
 }
